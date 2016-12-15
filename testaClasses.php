e717f70f974414cbe1dc10f940c27c4cec9ef6c8
luizbweb <luizbweb@gmail.com>
2016-12-15 15:53:10 -0200

    Implementando Connection Factory

---
+++ testaClasses.php
@@ -0,0 +1,40 @@
+<?php
+
+	function __autoload( $classe ) {
+		include_once 'Classes/{$classe}.class.php';
+	}
+
+	$conecta = new PDOConnectionFactory();
+	$conecta->getConnection();
+
+	$email = 'luizbweb@gmail.com';
+
+	$sql = "SELECT * FROM `Alunos` WHERE `e-mail` LIKE '". $email ."' LIMIT 1";
+	$query = $conecta -> query($sql);
+
+	/* Adicionando os dados às variàveis */
+
+	while ($dados = mysqli_fetch_array($query)) {
+	    $aluno = $dados['nome'];
+	    $orientador = $dados['orientador'];
+	    $nomeAtividade = $dados['atividade'];
+	}
+
+	$conecta->Close();
+
+	$participante = new Participante();
+	$participante->nome = $aluno;
+	$participante->email = $email;
+	$participante->id = 0;
+	$atividade = new Atividade();
+	$atividade->insereParticipante( $participante );
+	$atividade->insereNomeAtividade( $nomeAtividade );
+	$atividade->insereOrganizador( $orientador );
+	$atividade->insereDataAtividade('02/11/2016');
+	$atividade->insereHoraAtividade('12:00');
+	$atividade->insereLocal('UEZO');
+	$cert = new Certificado( $atividade, '0');
+	$cert->geraCertificado();
+
+
+?>
