5c925ef66e92031f04f58aae5a9677efde4cb396
luizbweb <luizbweb@gmail.com>
2017-01-23 15:58:57 -0200

    Utiliza os metodos para buscar atividade e participante

--- Actions/actionGerarCertificado.php
+++ Actions/actionGerarCertificado.php
@@ -4,16 +4,32 @@
  * Action para gerar os Certificados
  * Recebe dados dos campos atividade, email do participante.
  * Consulta dos dados da atividade no banco de dados e gera o certificado.
+ * Pode ser melhorado criando uma unica query que busque o participante de acordo com a atividade.
  */
 
 include_once '../Classes/Atividade.class.php';
 include_once '../Classes/Participante.class.php';
 
+// Recebe os dados
 $nome_atividade = $_POST['atividade'];
 $email = $_POST['email'];
 
+// Consulta as informações da atividade
+
+$atividade = new Atividade();
+$atividade->retornaAtividade( $nome_atividade );
+
+// Busca informações do Participante com base no email
+
+$participante = new Participante();
+$participante->retornaParticipante( $email, $participante->idAtividade );
+
+// Gerar o certificado
+
+
 
 // Script antigo:
+/*
 $atividade = new Atividade();
 $atividade->insereNomeAtividade( $nome_atividade );
 $atividade->insereLocal( $local );
@@ -29,11 +45,10 @@ $participante = new Participante();
 $participante->nome = $organizador;
 $participante->senha = $senha;
 $participante->email = $email;
+*/
 
 // Associa o organizador como primeiro Participante
 $atividade->insereParticipante( $participante );
-
-/*
 
 // Exibe os dados inseridos
 $testa[0] = $atividade->retornaAtividade();
@@ -48,19 +63,22 @@ foreach ($testa as $retornos) {
     echo '<br>';
 }
 echo '<br>';
-// $atividade->exibeParticipantes();
 
-*/
+$atividade->exibeParticipantes();
+
+/*
 
 $atividade->registraAtividade();
 $id_atividade = $atividade->retornaIdAtividade( $nome_atividade );
+*/
+
 
 ?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="refresh" content="3;../inserirParticipantes.php?atividade=<?php echo $id_atividade; ?>&n=0 "
-        <title>Atividade Registrada!</title>
+        <title>Certificado</title>
     </head>
     <body>
 
